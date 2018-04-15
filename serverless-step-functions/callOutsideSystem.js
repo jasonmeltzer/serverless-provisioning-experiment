80dce713c6bf23d33aafbb39c7c3601014a1a10e
Jason Meltzer <jmeltzer@gmail.com>
2018-04-15 15:42:31 -0700

    Introduced the concept of retries to one of the step functions. One failure is specific, the other is a generic/catch-all

--- serverless-step-functions/callOutsideSystem.js
+++ serverless-step-functions/callOutsideSystem.js
@@ -11,6 +11,19 @@ module.exports.call = (event, context, callback) => {
 	 var input = JSON.parse(eventText);
 	 var inputData = JSON.parse(input.input);
 	 
+	 // The intention of this function is to simulate calling out to an outside system.
+	 // Another goal is to use it to demonstrate how to handle retries, and to output a value used to make a 'choice'
+	 // in a state machine.
+	 // In order to force retries, we'll make the function fail sometimes.
+	 if (inputData.username && inputData.username.indexOf("fail1") > -1) { // Simulate failure if username contains 'fail1'
+		 var error = new Error('Failed when calling external system');
+		 error.name = 'external-failure';
+		 callback(error);
+	 } else if (inputData.username && inputData.username.indexOf("fail2") > -1) { // Simulate a misc failure if username contains 'fail2'
+	     callback("some other error");
+	 }
+	 
+	 
 	 var dynamoParams = {
          TableName : process.env.DYNAMODB_TABLE_MBOX,
 		 Key:{
@@ -37,6 +50,6 @@ module.exports.call = (event, context, callback) => {
 		 nextInputArr['username'] = inputData.username;
 	 }
 	 
-	 
+	 // Give back a random number so that the state machine can do something with it to demonstrate a "choice"
 	 callback(null, {"value": Math.random(), "input": JSON.stringify(nextInputArr)} )
 };
