fd01107af571b079c72d8492a0dc201b5d9397d2
Jason Meltzer <jmeltzer@gmail.com>
2018-04-23 20:10:05 -0700

    More progress on being able to interject a manual task in the middle of a workflow. Right now success vs failure is randomly decided - need to fix that next. There's also some weird timing around exactly when the task is sent off and whether it gets picked up. That might just be a byproduct of deploying the code frequently. Will have to look into it later.

--- serverless-step-functions-java/src/main/java/com/example/WaitForDeleteConfirmation.java
+++ serverless-step-functions-java/src/main/java/com/example/WaitForDeleteConfirmation.java
@@ -34,17 +34,29 @@ public class WaitForDeleteConfirmation implements RequestHandler<Map<String, Obj
 		builder.setClientConfiguration(new ClientConfiguration().withSocketTimeout(1000));
 		AWSStepFunctions stepFunctionsClient = builder.build();
 		
-		ListActivitiesResult listResult = stepFunctionsClient.listActivities(new ListActivitiesRequest());
-		LOG.info("Received list result: " + listResult.toString());
-		
 		GetActivityTaskRequest taskRequest = new GetActivityTaskRequest();
-		taskRequest.setActivityArn("arn:aws:states:us-east-1:083440467681:activity:provisioning-steps-java-dev-waitForDeleteConfirmationActivity"); 
-        //TODO genericize ARN
-		
+		taskRequest.setActivityArn(System.getenv("delete_confirmation_activity_arn")); 
+
 		try 
 		{
 			GetActivityTaskResult result = stepFunctionsClient.getActivityTask(taskRequest);
 			LOG.info("Received task result: " + result.toString());
+			
+			// Randomly decide whether to send back a success or failure, for now. These will later become part of
+			// REST endpoints that send a success or failure based on a link click in an email.
+			if ((new java.util.Random()).nextBoolean()) 
+			{
+				LOG.info("Sending task success");
+				SendTaskSuccessRequest taskSuccessRequest = new SendTaskSuccessRequest().withTaskToken(result.getTaskToken()).withOutput("{}");
+				stepFunctionsClient.sendTaskSuccess(taskSuccessRequest);
+			} 
+			else 
+			{
+				LOG.info("Sending task failure");
+				SendTaskFailureRequest taskFailureRequest = new SendTaskFailureRequest().withTaskToken(result.getTaskToken()).withError("TaskFAIL");
+				stepFunctionsClient.sendTaskFailure(taskFailureRequest);
+			}
+			
 		}
 		catch (com.amazonaws.SdkClientException e) // read timeout is expected if there aren't any events pending
 		{
